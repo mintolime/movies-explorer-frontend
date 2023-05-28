80426318fed5fcdf227820f10f63c66b2ee358bf
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-05-28 22:05:33 +0700

    dev: created techs comp

---
+++ movie-app/src/components/Techs/Techs.jsx
@@ -0,0 +1,40 @@
+import '../Techs/Techs.css'
+
+function Techs() {
+  return (
+    <section className='techs'>
+      <div className="content__inner">
+        <p className='content__about'>Технологии</p>
+      </div>
+      <h2 className='techs__title'>7 технологий</h2>
+      <p className='techs__subtitle'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      <ul className="techs__list">
+        <li className="techs__item">
+          <p className="techs__stek_text">HTML</p>
+        </li>
+        <li className="techs__item">
+          <p className="techs__stek_text">CSS</p>
+        </li>
+        <li className="techs__item">
+          <p className="techs__stek_text">JS</p>
+        </li>
+        <li className="techs__item">
+          <p className="techs__stek_text">React</p>
+        </li>
+        <li className="techs__item">
+          <p className="techs__stek_text">Git</p>
+        </li>
+        <li className="techs__item">
+          <p className="techs__stek_text">Express.js</p>
+        </li>
+        <li className="techs__item">
+          <p className="techs__stek_text">mongoDB</p>
+        </li>
+
+      </ul>
+    </section>
+
+  );
+}
+
+export default Techs;
