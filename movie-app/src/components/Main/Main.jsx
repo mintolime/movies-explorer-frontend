0c2d722dba08820e501faf3be00aab415d63668a
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-05-28 21:23:44 +0700

    dev: layout on different ext. AboutProject and Promo comp

--- movie-app/src/components/Main/Main.jsx
+++ movie-app/src/components/Main/Main.jsx
@@ -1,19 +1,14 @@
-import globus from '../../images/globus.png';
 import '../Main/Main.css'
+import Promo from '../Promo/Promo';
+import AboutProgect from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 function Main() {
   return (
     <main className="content">
-      <section aria-label="стартовая страница о проекте" className="promo">
-        <img className="promo__illustration" src={globus} alt="Логотип веба в форме глобуса" />
-        <div className="promo__context">
-          <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-          <p className="promo__subtitle">
-            Листайте ниже, чтобы узнать больше про этот проект и его создателя.
-          </p>
-           <button className='button button_type_about' ><a className="promo__link" href="#">Узнать больше</a></button>
-        </div>
-      </section>
+      <Promo />
+      <AboutProgect />
+      <Techs />
     </main>
   );
 }
---
+++ movie-app/src/components/Promo/Promo.jsx
@@ -0,0 +1,19 @@
+import globus from '../../images/globus.png';
+import '../Promo/Promo.css';
+
+function Promo() {
+  return (
+    <section aria-label="стартовая страница  «О проекте»" className="promo">
+      <img className="promo__illustration" src={globus} alt="Логотип веба в форме глобуса" />
+      <div className="promo__context">
+        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+        <p className="promo__subtitle">
+          Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+        </p>
+        <button className='button button_type_about' ><a className="promo__link" href="#">Узнать больше</a></button>
+      </div>
+    </section>
+  );
+}
+
+export default Promo;
