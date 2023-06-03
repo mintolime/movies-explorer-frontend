37ff097f903ab169f99e7720f45e0e694c7b2d0f
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-06-03 16:51:47 +0700

    fix: fixed duration movie

--- movie-app/src/components/MoviesCard/MoviesCard.jsx
+++ movie-app/src/components/MoviesCard/MoviesCard.jsx
@@ -1,11 +1,13 @@
 import '../MoviesCard/MoviesCard.css'
-
+import { getTimeFromMins } from '../../utils/getTimeFromMins';
 function MoviesCard({ movie }) {
+  // let time = movie.duration;
+  // console.log(time)
   return (
     <li className="movies__item">
       <div className="movies__heading">
         <h2 className="movies__title">{movie.nameRU}</h2>
-        <span className="movies__time">{`${movie.duration} мин`} </span>
+        <span className="movies__time">{getTimeFromMins(movie.duration)}</span>
       </div>
       <img
         className="movies__image"
@@ -14,12 +16,12 @@ function MoviesCard({ movie }) {
       />
       {/* <div className="movies__inner"> */}
 
-        {/* <div className="movies__items"> */}
-          <button
-            className="button button_type_save"
-            type="button">Сохранить</button>
+      {/* <div className="movies__items"> */}
+      <button
+        className="button button_type_save"
+        type="button">Сохранить</button>
 
-        {/* </div> */}
+      {/* </div> */}
       {/* </div> */}
       {/* <button className="button button_type_delete" /> */}
     </li>
