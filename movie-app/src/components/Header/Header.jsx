099f68516053733cab6365fac2e7f26d04a156ed
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-06-03 13:00:07 +0700

    dev: added checkbox on search-form

--- movie-app/src/components/Header/Header.jsx
+++ movie-app/src/components/Header/Header.jsx
@@ -3,11 +3,11 @@ import '../Header/Header.css'
 
 function Header() {
   return (
-    <header className="header header_login">
+    <header className="header header_white">
       <img className="logo logo_place_header " src={logo} alt="логотип шапки сайта" />
       <div className="header__nav-box">
-        <button className='button button_type_auth'>Регистрация</button>
-        <button className='button button_type_login'>Войти</button>
+        {/* <button className='button button_type_auth'>Регистрация</button>
+        <button className='button button_type_login'>Войти</button> */}
       </div>
     </header>
   );
---
+++ movie-app/src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,33 @@
+import '../SearchForm/SearchForm.css'
+import useFormAndValidation from '../../hooks/useFormAndValidation';
+import find from '../../images/icon-find.png';
+function SearchForm() {
+
+  const { values, handleChange } = useFormAndValidation();
+  return (
+    <section className="search-form">
+      <form
+        className="search-form__inner"
+      >
+        <input
+          className="search-form__input"
+          type="text"
+          placeholder='Фильмы'
+          value={values.movies || ''}
+          onChange={handleChange}
+          name="movies"
+          aria-label="Поиск фильмов">
+        </input>
+        <button className="button button_type_search" type="submit"><img className="search-form__icon" src={find} alt="иконка поиска" /></button>
+      </form>
+
+      <div className="search-form__toogle">
+        <p className="search-form__text">Короткометражки</p>
+           <input class="search-form__checkbox" type="checkbox" id="switch" /><label class="search-form__label" for="switch"></label>
+      </div>
+
+    </section>
+  );
+}
+
+export default SearchForm;
