39c01892cdb37b06aee7cd55ee9511e0f205fd4a
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-06-07 21:16:52 +0700

    fix: redacted layout on 320px >1280px

--- movie-app/src/components/PageNotFound/PageNotFound.jsx
+++ movie-app/src/components/PageNotFound/PageNotFound.jsx
@@ -4,16 +4,17 @@ import '../PageNotFound/PageNotFound.css';
 
 function PageNotFound() {
   return (
-    <div className="not-found">
+    <section className="not-found">
       <div className="not-found__inner">
-        <h2 className="not-found__heading">404</h2>
+      <div className="not-found__text">
+         <h2 className="not-found__heading">404</h2>
         <p className="not-found__subtitle">Страница не найдена</p>
       </div>
-      <Link to="/" className="not-found__link">
+  <Link to="/" className="not-found__link">
         Назад
       </Link>
-      {/* <button className='button button_type_back'>Назад</button> */}
-    </div>
+      </div>
+    </section>
   );
 }
 
