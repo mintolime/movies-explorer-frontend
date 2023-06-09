87a138ab165c51a26b14d37f82b9fe80891e6902
mintolime <104442896+mintolime@users.noreply.github.com>
2023-06-09 02:50:53 +0000

    fix: fixed checkbox-toogle on navigation

--- movie-app/src/components/Navigation/Navigation.jsx
+++ movie-app/src/components/Navigation/Navigation.jsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
+import React from 'react'
 
 import icon from '../../images/icon-profile.png'
 import '../Navigation/Navigation.css'
 
 function Navigation() {
+  const [isToggle, setIsToggle] = React.useState(false)
+
   return (
-    <nav role="navigation" className='navigation'>
+    <nav role="navigation" className={`navigation ${isToggle? '' : 'navigation_no-active'}`}>
       <div className='navigation__menu'>
-        <input type="checkbox" className='navigation__checkbox' />
+        <input type="checkbox" className='navigation__checkbox' onClick={() => setIsToggle(!isToggle)}  />
         <span className='navigation__toogle' ></span>
         <span className='navigation__toogle'></span>
         <span className='navigation__toogle'></span>
