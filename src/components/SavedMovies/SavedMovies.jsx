b439faa03c10a3678b3e4aa00641bafb3c6e9a4d
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-06-12 09:34:37 +0700

    fixed : added SearchForm on SavedMoves

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -2,10 +2,12 @@
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import { moviesData } from '../../utils/movies';
+import SearchForm from '../SearchForm/SearchForm';
 
 function SavedMovies() {
   return (
     <>
+      <SearchForm />
       <MoviesCardList />
       {/* <MoviesCard movie={moviesData}/> */}
     </>
