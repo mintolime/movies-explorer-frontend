9687357bc283dbe1e97f787fdb0cb70cdd55982b
Hi-lo <104442896+mintolime@users.noreply.github.com>
2023-06-25 04:18:27 +0000

    dev: update a block result

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -2,15 +2,26 @@ import '../MoviesCardList/MoviesCardList.css';
 // import { moviesData } from '../../utils/movies';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import Button from '../Button/Button';
+import { useResize } from '../../hooks/useResize';
 
-function MoviesCardList({ moviesData, searchActive,onSaveMovie }) {
-  // console.log('moviesData',searchActive)
+function MoviesCardList({ moviesData, searchActive, onSaveMovie, onDelete }) {
+  const size = useResize();
   return (
     <section className="movies" aria-label="галерея фильмов пользователя">
       <ul className="movies__list">
-        {moviesData.map((movie) => (
-          <MoviesCard movie={movie} key={movie.id} onSaveMovie={onSaveMovie}/>
-        ))}
+        {size.isScreenSm
+          ? moviesData?.slice(0, 5).map((movie) => (
+              <MoviesCard movie={movie} key={movie.id || movie.movieId} onSaveMovie={onSaveMovie} onDelete={onDelete} />
+            ))
+          : size.isScreenMd
+          ? moviesData?.slice(0, 8).map((movie) => (
+              <MoviesCard movie={movie} key={movie.id || movie.movieId} onSaveMovie={onSaveMovie} onDelete={onDelete} />
+            ))
+          : size.isScreenXl
+          ? moviesData?.slice(0, 12).map((movie) => (
+              <MoviesCard movie={movie} key={movie.id || movie.movieId} onSaveMovie={onSaveMovie} onDelete={onDelete} />
+            ))
+          : null}
       </ul>
       {searchActive && <Button btnClass="button_type_more" btnType="button" btnText="Ещё" />}
     </section>
